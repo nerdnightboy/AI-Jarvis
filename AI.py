e8cbd29a52c7ad33faedd1bdc43cfc9f2e76085f
nerdnightboy <nerdnightboy@gmail.com>
2022-11-07 22:33:15 +0530

    AI Project

---
+++ AI.py
@@ -0,0 +1,104 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import webbrowser
+import os
+import smtplib
+import wikipedia
+import random
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour <18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am Elyssa and pretty sure your personal girlfriend, Please tell me how may I help you?")
+
+def takeCommand():
+    # It take microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, Language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please....")
+        return "None "
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('nerdnightboy2.0@gmail.com', 'your-password')
+    server.sendmail('nerdnightboy2.0@gmail.com', to, content)
+    server.close()
+
+if __name__=="__main__":
+    wishMe()
+    while(True):
+    # if 1:
+        query = takeCommand().lower()
+        # Logic for executing tasks based on query
+        if'wikipedia' in query:
+            speak("Searching Wikipedia....")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 2)
+            speak("Accourding to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'play music' in query:
+            music_dir = 'E:\\Videos'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\nerdn\\AppData\\Local\\Programs\\Microsoft VS Code"
+            os.startfile(codePath)
+
+        elif 'email to nerdy' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "nerdnightboy2.0@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!.")
+            except Exception as e:
+                print(e)
+                speak("Sorry dude! I don't know what the fuck just happend but your email is bullshit and I lost it man!!")
